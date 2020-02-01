592c3fcf2093f89361db2d7de8e7747f714c0ad3
Oliver Jørgensen <oljo@itu.dk>
2020-02-01 12:28:46 +0100

    Scrrriiiiiiiiiiiipts

---
+++ GGJ 2020 V2/Assets/Scripts/GiveItemCommand.cs
@@ -0,0 +1,8 @@
+﻿public class GiveItemCommand : Command
+{
+    public Patient Patient;
+
+    public override void ExecuteCommand(Player avatar)
+    {
+    }
+}
---
+++ GGJ 2020 V2/Assets/Scripts/Patient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patient : MonoBehaviour
+{
+    // How much blood to lose per second
+    public float BloodLossRate = 5f;
+
+    public GameEvent PatientDead;
+
+    public SpriteRenderer BloodSprite;
+
+    public float Blood = 100f;
+
+    public float MaxBlood = 100f;
+
+    private float _bloodHeight;
+
+    void Awake() {
+        _bloodHeight = BloodSprite.size.y;
+    }
+
+    void Update()
+    {
+        Blood -= BloodLossRate * Time.deltaTime;
+
+        if (Blood <= 0f) {
+            PatientDead.Raise();
+        }
+
+        BloodSprite.size = Vector2.Lerp(new Vector2(BloodSprite.size.x, 0f), new Vector2(BloodSprite.size.x, _bloodHeight), Blood / MaxBlood);
+    }
+
+    public void Heal(float healAmount)
+    {
+        Blood = Mathf.Min(Blood + healAmount, 100.0f);
+    }
+}
