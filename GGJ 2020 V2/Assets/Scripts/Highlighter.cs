92ee714de53735cc7e86b9993919c7e003c2c479
Oliver Jørgensen <oljo@itu.dk>
2020-02-01 17:32:01 +0100

    I need this

---
+++ GGJ 2020 V2/Assets/Scripts/Highlighter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Highlighter : MonoBehaviour
+{
+    public Camera Camera;
+
+    private Vector2 _mousePos;
+
+    private SpriteRenderer _lastHit;
+    
+
+    void Update() {
+        var ray = Camera.ScreenPointToRay(Mouse.current.position.ReadValue(), Vector2.zero);
+        var hit = Physics2D.Raycast(ray.direction, Vector2.zero);
+
+        if (hit.collider != null) {
+            if (_lastHit != null) {
+                if (_lastHit.gameObject != hit.collider.gameObject) {
+
+                }
+            }
+        }
+    }
+}
