4a74e2b4728fedfd4a9444bcfd77741460fd0f8b
Oliver Jørgensen <oljo@itu.dk>
2020-02-01 20:13:40 +0100

    error thing

--- GGJ 2020 V2/Assets/Scripts/Highlighter.cs
+++ GGJ 2020 V2/Assets/Scripts/Highlighter.cs
@@ -17,7 +17,6 @@ public class Highlighter : MonoBehaviour
         var ray = Camera.ScreenPointToRay(Mouse.current.position.ReadValue());
         var hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, LayerMask.GetMask("Highlightable") | LayerMask.GetMask("Both"));
 
-        Debug.Log(_lastHit?.material.shader.GetPropertyName(2));
         if (hit.collider != null) {
             if (_lastHit != null) {
                 if (_lastHit.gameObject != hit.collider.gameObject) {
