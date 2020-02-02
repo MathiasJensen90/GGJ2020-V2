9f921e4a412af7c9e29502a55ecf170f9dac2a7b
Mathias B <mathiasboldsen@gmail.com>
2020-02-02 13:29:25 +0100

    juuuiiiice!

---
+++ GGJ 2020 V2/Assets/Scripts/DeleteJuice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteJuice : MonoBehaviour
+{
+
+  
+
+
+    public void startCour()
+    {
+        StartCoroutine(hereIsJuice()); 
+    }
+
+
+    private IEnumerator hereIsJuice ()
+        {
+        yield return new WaitForSeconds(3);
+        gameObject.SetActive(false); 
+        }
+
+  
+}
