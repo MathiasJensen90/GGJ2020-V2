489d1f6ee1c6b33092c248397c6e94d567374e1a
Oliver Jørgensen <oljo@itu.dk>
2020-02-02 13:58:22 +0100

    fixed display

--- GGJ 2020 V2/Assets/Scripts/SavedPatientsDisplay.cs
+++ GGJ 2020 V2/Assets/Scripts/SavedPatientsDisplay.cs
@@ -8,20 +8,9 @@ public class SavedPatientsDisplay : MonoBehaviour
     public TMPro.TextMeshProUGUI Text;
     public ScoreManager _SManager;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void UpdateText()
-    {
         Text.text = _SManager.PatientsCured.ToString();
     }
-
 }
