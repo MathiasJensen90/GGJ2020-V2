adeccfeb5eefe3c416ad68a0c0f80f23bf7137f0
Mathias B <mathiasboldsen@gmail.com>
2020-02-02 11:53:28 +0100

    clicksound

---
+++ GGJ 2020 V2/Assets/Scripts/ClickSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+
+    public AudioClip[] clickSound = new AudioClip[3];
+ 
+
+    
+    public void Playclick()
+    {
+        int randomInt = Random.Range(0, clickSound.Length);
+        AudioSource.PlayClipAtPoint(clickSound[randomInt], transform.position, 0.8f);
+    }
+}
