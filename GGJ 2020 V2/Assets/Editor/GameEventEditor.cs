7e44d38706cfa2cc6193153fe78ea13fc47e1b5f
Oliver Jørgensen <oljo@itu.dk>
2020-02-02 09:45:51 +0100

    GameEvent editor script

---
+++ GGJ 2020 V2/Assets/Editor/GameEventEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameEvent))]
+public class GameEventEditor : Editor
+{
+    public GameObject Target;
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        var gameEvent = (GameEvent) target;
+
+        Target = (GameObject) EditorGUILayout.ObjectField(Target, typeof(GameObject), allowSceneObjects: true);
+        if (GUILayout.Button("Raise Event!")) {
+            gameEvent.Raise(Target); 
+        }
+    }
+}
