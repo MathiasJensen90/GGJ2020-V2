2f83c468f22c1cfe0766e0ff4d9173735dddd41a
Oliver Jørgensen <oljo@itu.dk>
2020-02-02 10:24:15 +0100

    game over scene wauw

---
+++ GGJ 2020 V2/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public ScoreManager ScoreManager;
+
+    public TMPro.TextMeshProUGUI Text;
+
+    public void Awake() {
+        Text.text = $"You cured: {ScoreManager.PatientsCured}";
+    }
+
+    public void GoToMainMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
