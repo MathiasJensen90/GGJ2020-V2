d14adb9655e55f883dd6201af06b87a990d6990a
MathiB <mathiasboldsen@gmail.com>
2020-02-09 21:18:11 +0100

    Test of stuff

---
+++ GGJ 2020 V2/Assets/Scripts/MathiasScripts/ItemDB.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+
+    public List<ItemExample> itemDatabase = new List<ItemExample>();
+
+    private void Start()
+    {
+       // itemDatabase[0]
+    }
+}
